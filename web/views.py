2f6785d51e646046044ad1a3961311aefc6c6f1c
EdmundoTriguero <edmundotriguero@gmail.com>
2023-03-09 01:55:24 -0400

    Final con inicio aleatorio finalizado

--- web/views.py
+++ web/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 from apanel.models import Slider
 
+from ecommerce.models import Producto
+
+import random
+
 # Create your views here.
 
 
@@ -14,7 +18,31 @@ def inicio(request):
     if request.method == 'GET':
         obj = Slider.objects.filter(estado=True).all().order_by('orden')
 
-        contexto = {'obj':obj}
+
+        cam = Producto.objects.filter(categoria_id=1, estado=True).all()
+
+        tra = Producto.objects.filter(categoria_id=3, estado=True).all()
+
+        nov = Producto.objects.filter(categoria_id=2, estado=True).all()
+
+
+        obj_cam = []
+
+
+        tra_ale = random.choice(tra)
+
+        nov_ale = random.choice(nov)
+
+        for num in range(1, 5):
+            aletory = random.choice(cam)
+            obj_aux = {}
+            obj_aux["nombre"] = aletory.nombre
+            obj_aux["img"] = str(aletory.img)
+
+            obj_cam.append(obj_aux)
+
+
+        contexto = {'obj':obj,'cam':obj_cam,'tra':tra_ale,'nov':nov_ale}
 
     return render(request, template_name, contexto)
 
